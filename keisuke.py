c3824f50eec04140261cb66653de1be35e266ca8
imranariffin <imranariffin@yahoo.com>
2016-04-07 14:53:20 -0400

    done defining csp variables. next: define constraint

--- keisuke.py
+++ keisuke.py
@@ -45,7 +45,6 @@ def create_keisuke_puzzle(n):
     print(vertical)
     return final_board, horizontal, vertical
 
-
 def create_keisuke_puzzle_2(n):
     '''
     Creates a randomly valid keisuke puzzle that is of dimension n x n.
@@ -90,5 +89,50 @@ def create_keisuke_puzzle_2(n):
 
     return final_board, horizontal, vertical
 
+def create_keisuke_puzzle_3(n):
+    '''
+    Creates a randomly valid keisuke puzzle that is of dimension n x n.
+    Will return a list representation of the puzzle along with the list of numeric values.
+
+    Possible version where we adjust the occurrence of black squares.
+    '''
+
+    quarter = n / 4
+    percent = quarter / n
+    possible_values = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    game_board = [[choice(possible_values) if random() >= percent else -1 for x in range (n)] for y in range(n)]
+
+    horizontal = []
+    vertical = []
+
+    for row in game_board:
+        temp = ()
+        for item in row:
+            if item == -1:
+                if temp != () and len(temp) > 1:
+                    horizontal.append(temp)
+                temp = ()
+            else:
+                temp += (item,)
+        if temp != () and len(temp) > 1:
+            horizontal.append(temp)
+
+    for i in range(n):
+        temp = ()
+        for row in game_board:
+            if row[i] == -1:
+                if temp != () and len(temp) > 1:
+                    vertical.append(temp)
+                temp = ()
+            else:
+                temp += (row[i],)
+        if temp != () and len(temp) > 1:
+            vertical.append(temp)
+
+    # final_board = [[item if item == -1 else 0 for item in row] for row in game_board]
+
+    return game_board, horizontal, vertical
+
 if __name__ == '__main__':
     create_keisuke_puzzle(5)
+
