865ba9b95a3f08752b3688b75f3cd5ff0737e267
sheltonli <shelton.li.ts@gmail.com>
2016-04-08 23:40:09 -0400

    fixed up tests

--- keisuke_tests.py
+++ keisuke_tests.py
@@ -3,14 +3,30 @@ from propagators import *
 import sys
 
 
-if __name__ == '__main__':
-    sys.setrecursionlimit(3000)
-    p = create_keisuke_puzzle(10)
+def run_puzzle(n):
+    p = create_keisuke_puzzle(n)
     csp,var_array = keisuke_csp_model_1(p[0], p[1], p[2])
 
     solver = BT(csp)
     print("GAC")
     solver.bt_search(prop_GAC)
     print("Solution")
-    # print_sudo_soln(var_array)
-    # print("===========")
+    print_sudo_soln(var_array)
+    print("===========")
+
+
+def run_hard_puzzle(n, m):
+    p = create_keisuke_puzzle_hard(n, m)
+    csp,var_array = keisuke_csp_model_1(p[0], p[1], p[2])
+
+    solver = BT(csp)
+    print("GAC")
+    solver.bt_search(prop_GAC)
+    print("Solution")
+    print_sudo_soln(var_array)
+    print("===========")
+
+if __name__ == '__main__':
+    sys.setrecursionlimit(3000)
+    run_puzzle(5)
+    # run_hard_puzzle(5, 2)
